1125a5ab8a50dd0019971ab58b9b68b87b085c70
Nitish <nitishk642@gmail.com>
2025-08-18 23:05:41 +0530

    feat add email verification

--- src/helper/sendVerificationEmail.ts
+++ src/helper/sendVerificationEmail.ts
@@ -1,2 +1,29 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../Emails/verificationEmails";
+import { ApiResponse } from "../types/ApiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: email,
+      subject: "Mystry message | Verification Code",
+      react: VerificationEmail({ userName: username, otp: verifyCode }),
+    });
+    return {
+      success: true,
+      message: "Verification email sent successfully",
+      isAcceptingMessages: true,
+    };
+  } catch (emailError) {
+    console.error("Error sending verification email", emailError);
+    return {
+      success: false,
+      message: "Error sending verification email",
+    };
+  }
+}
